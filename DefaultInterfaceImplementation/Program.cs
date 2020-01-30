6530ce94f630500a782430663b47f128f79a4f7a
Ashish <Ashish.Rathi@KLINGELNBERG.com>
2020-01-30 09:35:38 +0530

    Solution with empty projects added for explaining different features of C# 8.0

---
+++ DefaultInterfaceImplementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DefaultInterfaceImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
